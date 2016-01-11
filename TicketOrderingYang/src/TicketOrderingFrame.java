058d6a65d99f07dba825e201169f1c6cd0b9e41e
sophyyang <sophyyang@users.noreply.github.com>
2016-01-10 22:39:58 -0600

    Update TicketOrderingFrame.java

--- TicketOrderingYang/src/TicketOrderingFrame.java
+++ TicketOrderingYang/src/TicketOrderingFrame.java
@@ -3,10 +3,14 @@
  * Date: 2016-01-09
  */
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -23,7 +27,10 @@ public class TicketOrderingFrame extends JFrame {
 	JTextField amountT = new JTextField(10);
 
 	public TicketOrderingFrame() {
-		JPanel panel = new JPanel();
+		JFrame frame = new JFrame();
+		JPanel panel1 = new JPanel();
+		JPanel panel2 = new JPanel();
+		JPanel panel3 = new JPanel();
 		JButton b = new JButton("Purchase tickets");
 		JLabel orderMsg = new JLabel("Enter how many tickets you'd like to purchase (1-6):");
 
@@ -32,13 +39,19 @@ public class TicketOrderingFrame extends JFrame {
 		ActionListener d = new submitClickListener();
 		b.addActionListener(d);
 
-		panel.add(orderMsg);
-		panel.add(amountT);
-		panel.add(b);
-		panel.add(errorL);
-		panel.add(bar);
-		panel.add(soldOut);
-		add(panel);
+		panel1.add(orderMsg);
+		panel2.add(amountT);
+		panel2.add(b);
+		panel2.add(errorL);
+		panel2.add(bar);
+		panel3.add(soldOut);
+		frame.getContentPane().add(BorderLayout.NORTH, panel1);
+		frame.getContentPane().add(BorderLayout.CENTER, panel2);
+		frame.getContentPane().add(BorderLayout.SOUTH, panel3);
+		frame.setSize(400, 250);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
 
 	}
 
@@ -48,6 +61,7 @@ public class TicketOrderingFrame extends JFrame {
 		public void actionPerformed(ActionEvent arg0) {
 			// TODO Auto-generated method stub
 			try {
+				Font bigFont = new Font("serif", Font.BOLD, 28);
 				errorL.setText("");
 				myOrder.decreaseTicket(Integer.parseInt(amountT.getText()));
 				errorL.setText(myOrder.getErrorMsg());
@@ -61,12 +75,16 @@ public class TicketOrderingFrame extends JFrame {
 				bar.setNewValues(x, y, newColor);
 				if (x == 0) {
 					soldOut.setText("Event is sold out.");
+					soldOut.setFont(bigFont);
+					soldOut.setForeground(Color.red);
 				}
 
 			} catch (NumberFormatException ex) {
 				errorL.setText(noNumeric);
+				amountT.setText("");
 			}
 		}
 
 	}
 }
+
